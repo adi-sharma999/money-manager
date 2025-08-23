d814d28551684a7e3c032b92494327e8679dbc3b
Aditya Sharma <you@example.com>
2025-08-23 12:11:50 +0530

    controller update

--- src/main/java/in/aditya/moneymanager/controller/HomeController.java
+++ src/main/java/in/aditya/moneymanager/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/status","/health","/working"})
+@RequestMapping({"/status","/health"})
 public class HomeController {
 
     @GetMapping
